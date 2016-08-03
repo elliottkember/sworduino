d47aa735ecea83f684797d60e3637714b383b481
Elliott Kember <elliott.kember@gmail.com>
2016-08-03 11:30:23 +0000

    Renamed dave's pattern's variables

--- staff/staff.ino
+++ staff/staff.ino
@@ -47,26 +47,26 @@ void setup() {
   }
 }
 
-uint8_t offset = 0;
-int ms = 0;
+uint8_t daveOffset = 0;
+int daveMilliseconds = 0;
 
 // Dave's pattern. By Dave Grijalva with love.
 // A gradient background, with rotating bright points and white sparkles.
 void dave() {
   for (int k=0; k<NUM_LEDS-1; k++) {
     uint8_t k8 = k;
-    uint8_t hue = (k/15 * 255) + (offset*5);
-    uint8_t saturation = rand() % 255 > 252 ? 0 : 255; //NUM_LEDS - (k+offset*10);
-    uint8_t value = k8 % 14 == offset % 14 ? 200 : 40; //NUM_LEDS- (k+offset*10);
+    uint8_t hue = (k/15 * 255) + (daveOffset*5);
+    uint8_t saturation = rand() % 255 > 252 ? 0 : 255; //NUM_LEDS - (k+daveOffset*10);
+    uint8_t value = k8 % 14 == daveOffset % 14 ? 200 : 40; //NUM_LEDS- (k+daveOffset*10);
     if(saturation == 0){
       value = 255;
     }
     leds[k] = CHSV(hue, saturation, value);
   }
   EVERY_N_MILLISECONDS(1000/24){
-    ms++;
-    if(ms % 3 == 0){
-      offset++;
+    daveMilliseconds++;
+    if(daveMilliseconds % 3 == 0){
+      daveOffset++;
     }
   }
 }
