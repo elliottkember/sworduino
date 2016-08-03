e673538f6ca037bf64cef34ae5fcdb9ec993d4bf
Elliott Kember <elliott.kember@gmail.com>
2016-08-03 11:32:15 +0000

    I should be passing some of these variables by reference

--- staff/staff.ino
+++ staff/staff.ino
@@ -73,6 +73,7 @@ void dave() {
 
 int numberOfSparkles = 1;
 bool increasing = true;
+// TODO increasing and numberOfSparkles should probably be passed by reference
 int upAndDownBy(int value, int difference, int max, int min) {
   if (value < max && increasing) {
     value += difference;
