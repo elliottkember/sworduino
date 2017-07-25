bb81629a886d22ad4396dafad569a72698b09f07
Elliott Kember <elliott.kember@gmail.com>
2017-07-24 23:12:09 -0700

    Lantern with a button

---
+++ lantern-button/lantern.ino
@@ -0,0 +1,65 @@
+#include "FastLED.h"
+
+#define LED_DT 7              // Data pin to connect to the strip.
+#define COLOR_ORDER GRB       // Are they RGB, GRB or what??
+#define LED_TYPE WS2812B // Don't forget to change LEDS.addLeds
+uint8_t MAX_BRIGHT = 255;     // Overall brightness definition. It can be changed on the fly.
+uint8_t hue = 50;                 // Starting hue.
+bool firstTimeRunningThroughPattern = true;
+#define NUM_LEDS 600         // Number of LED's.
+struct CRGB leds[NUM_LEDS];   // Initialize our LED array.
+
+void setup() {
+  Serial.begin(57600);
+  LEDS.addLeds<LED_TYPE, LED_DT, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness(MAX_BRIGHT);
+  set_max_power_in_volts_and_milliamps(5, 3000);
+  pinMode(14, INPUT_PULLUP);
+  digitalWrite(14, HIGH);
+  randomSeed(analogRead(0));
+  delay(50);
+}
+
+int on = true;
+int numberOfLoopsPressed = 0;
+int switchMode = 1;
+int numberOfBrightnesses = 6;
+int brightnesses [6] = { 0, 32, 64, 128, 200, 255 };
+int brightnessIndex = 0;
+uint8_t bright = 1;
+int16_t newBright = brightnesses[brightnessIndex];
+
+int getLevel() {
+  switchMode = digitalRead(14);
+
+  if (switchMode == 0) { 
+    numberOfLoopsPressed++;
+  } else {
+    if (numberOfLoopsPressed < 30 && numberOfLoopsPressed > 0) {
+      brightnessIndex = (brightnessIndex + 1) % 6;
+      newBright = brightnesses[brightnessIndex];
+    }
+    if (bright < newBright) {
+      bright += min(255 - bright, 5);
+    } else if (newBright == 0 && bright > 0) {
+      bright -= min(bright, 5);
+    }
+    numberOfLoopsPressed = 0;
+  }
+
+  if (bright > 255) bright = 255;
+  if (bright < 0) bright = 0;
+
+  return bright;
+}
+
+void loop () {
+//  lanternPattern(brightnessIndex);
+  fill_solid(leds, NUM_LEDS, CHSV(hue, 255, 255));
+  hue++;
+  Serial.println(hue);
+  FastLED.setBrightness(255);
+  FastLED.show();
+}
+
+
---
+++ lantern-button/lanternPattern.ino
@@ -0,0 +1,62 @@
+int numberOfSparkles = 1;
+bool increasing = true;
+
+int curve(int value, int difference, int maximum) {
+  if (value < maximum && increasing) {
+    value += difference;
+  } else if (value > 1) {
+    value -= difference;
+    increasing = false;
+  } else {
+    increasing = true;
+    value += difference;
+  }
+  return value;
+}
+
+int upAndDownBy(int value, int difference) {
+  return curve(value, difference, 20);
+}
+
+#define SNAKES_COUNT 10
+uint16_t snakes[SNAKES_COUNT];
+int snake = 0;
+
+void changePattern() {
+  hue += 180;
+}
+
+void lanternPattern(int level) {
+  EVERY_N_MILLISECONDS(20) {
+    for (int i = 0; i < SNAKES_COUNT; i++) {
+      uint16_t j = snakes[i];
+      if (j > 0) {
+        leds[j] = CHSV(hue + ((NUM_LEDS - j) / 15) - (j * level / 20), 240, 255);
+        snakes[i] += 8;
+        if (snakes[i] >= NUM_LEDS) snakes[i] = 0;
+      }
+    }
+  }
+
+  EVERY_N_MILLISECONDS(2000) {
+    snakes[snake] = random(7);
+    snake++;
+    if (snake >= SNAKES_COUNT) snake = 0;
+    numberOfSparkles = curve(numberOfSparkles, 1, NUM_LEDS * 0.01);
+  }
+
+  fadeToBlackBy(leds, NUM_LEDS, 8);
+  
+  for (int i = 0; i < numberOfSparkles; i++) {
+    int pos = random16(NUM_LEDS);
+    if (!leds[pos]) {
+      leds[pos] = CHSV(hue + (pos / 15) + (pos * level / 20), 240, 255);
+    }
+  }
+  
+  EVERY_N_MILLISECONDS(100) {
+    hue += 1;
+  }
+  
+//  delay(8);
+}
