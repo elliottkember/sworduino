13fc4cd691f0897506742f0d06c4593712515140
Elliott Kember <elliott.kember@gmail.com>
2017-08-05 13:48:07 +0000

    Simplification. New lantern style

--- baton/baton.ino
+++ baton/baton.ino
@@ -1,15 +1,12 @@
 #include "FastLED.h"
 
-// Digital unsigned subtraction macro. if result <0, then => 0. Otherwise, take on fixed value.
-#define qsubd(x, b)  ((x>b)?255:0)
-// Analog Unsigned subtraction macro. if result <0, then => 0
-#define qsuba(x, b)  ((x>b)?x-b:0)
-
+#define qsubd(x, b)  ((x>b)?255:0) // Digital unsigned subtraction macro. if result <0, then => 0. Otherwise, take on fixed value.
+#define qsuba(x, b)  ((x>b)?x-b:0) // Analog Unsigned subtraction macro. if result <0, then => 0
 #define LED_DT 7
 #define COLOR_ORDER GRB
 #define LED_TYPE WS2812B
 #define VOLTS 5
-#define MAX_CURRENT_IN_MA 200
+#define MAX_CURRENT_IN_MA 1000
 #define NUM_LEDS 300
 #define CIRCUMFERENCE 8
 #define ROTATION_IN_SECONDS 1
@@ -40,7 +37,7 @@ void setup() {
 
 void (*patterns[])() = {
   dave,
-  lanternPattern,
+  lantern,
   beautifulSparkles,
   shootingGradients,
   discoTwirl,
--- baton/lantern.ino
+++ baton/lantern.ino
@@ -1,6 +1,5 @@
 int numberOfSparkles = 0;
-
-int snakeDelay = 500;
+int snakeDelay = 2000;
 
 namespace Lantern {
 
@@ -8,42 +7,53 @@ namespace Lantern {
   uint16_t snakes[SNAKES_COUNT];
   int snake = 0;
   int hue = 0;
+  int snakeHueOffset = 20;
 
   void draw() {
+
+    // Draw snakes
     EVERY_N_MILLISECONDS(10) {
       for (int i = 0; i < SNAKES_COUNT; i++) {
         uint16_t j = snakes[i];
         if (j > 0 && j < NUM_LEDS) {
-          Global::leds[j] = CHSV(hue + ((NUM_LEDS - j) / 15), 240, 255);
-
+          int h = hue + ((NUM_LEDS - j) / 15) + snakeHueOffset;
+          Global::leds[j] = CHSV(h, 240, 255);
           snakes[i] += 8;
           if (snakes[i] >= NUM_LEDS) snakes[i] = 0;
         }
       }
-      numberOfSparkles = upAndDownBy(numberOfSparkles, 1, NUM_LEDS * 0.01, 0);
     }
 
+    EVERY_N_MILLISECONDS(80) {
+      numberOfSparkles = upAndDownBy(numberOfSparkles, 1, NUM_LEDS * 0.05, 0);
+    }
+
+    // Make a new snake
     EVERY_N_MILLISECONDS(snakeDelay) {
       snakes[snake] = random(7);
       snake++;
       if (snake >= SNAKES_COUNT) snake = 0;
     }
 
-    fadeToBlackBy(Global::leds, NUM_LEDS, 8);
+    // General fade
+    fadeToBlackBy(Global::leds, NUM_LEDS, 18);
 
+    // Draw sparkles
     for (int i = 0; i < numberOfSparkles; i++) {
       int pos = random16(NUM_LEDS);
-      if (!Global::leds[pos]) {
-        Global::leds[pos] = CHSV(hue + (pos / 15), 240, 128);
-      }
+      // if (!Global::leds[pos]) {
+      int brightness = 140 + (numberOfSparkles / NUM_LEDS * 115);
+        Global::leds[pos] = CHSV(hue + (pos / 15), 240, brightness) + random(10);
+      // }
     }
 
+    // Change colour
     EVERY_N_MILLISECONDS(100) {
-      hue += 1;
+      hue += 5;
     }
   }
 }
 
-void lanternPattern() {
+void lantern() {
   Lantern::draw();
 }
