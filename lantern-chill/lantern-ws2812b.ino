1dd5f08e87cc38e259433c154e5f79865082aee5
Elliott Kember <elliott.kember@gmail.com>
2017-12-18 22:44:39 -0800

    New chill lantern pattern from Lyft party

---
+++ lantern-chill/lantern-ws2812b.ino
@@ -0,0 +1,47 @@
+// This is a test of Lantern patterns with WS2812B LEDs on the big light.
+
+// #define FASTLED_ALLOW_INTERRUPTS 0 // https://github.com/FastLED/FastLED/wiki/Platform-limitations
+#include "FastLED.h"
+
+#define LED_DATA 7
+#define LED_CLOCK 11
+
+#define COLOR_ORDER GRB
+#define LED_TYPE WS2812B
+
+// APA102: #define COLOR_ORDER BGR
+
+#define apa102 false
+#define STARTING_BRIGHTNESS 255
+#define NUM_LEDS 1500
+#define SPI false
+
+struct CRGB leds[NUM_LEDS];
+
+void setup() {
+  if (apa102) {
+    if (SPI) {
+      // enable access to LEDs via SPI
+      pinMode(7, OUTPUT);
+      digitalWrite(7, HIGH);
+      FastLED.addLeds<APA102, COLOR_ORDER>(leds, NUM_LEDS);
+    } else {
+      FastLED.addLeds<APA102, LED_DATA, LED_CLOCK, COLOR_ORDER>(leds, NUM_LEDS);
+    }
+  } else {
+    FastLED.addLeds<WS2812B, LED_DATA, COLOR_ORDER>(leds, NUM_LEDS);
+  }
+
+  Serial.begin(57600);
+  set_max_power_in_volts_and_milliamps(5, 8000);
+  randomSeed(analogRead(0));
+  FastLED.setDither(0);
+  FastLED.delay(50);
+  FastLED.setBrightness(STARTING_BRIGHTNESS);
+  setupLanternPattern();
+}
+
+void loop() {
+  lanternPattern();
+  FastLED.show();
+}
--- lantern-chill/lanternPattern.ino
+++ lantern-chill/lanternPattern.ino
@@ -1,87 +1,73 @@
 #include <ArduinoJson.h>
+#include <WiFiUdp.h>
 
-DynamicJsonBuffer jsonBuffer(500);
+int numberOfSparkles = 0;
+bool increasing = true;
+#define CIRCUMFERENCE 8;
+#define SNAKES_COUNT 40
+uint8_t hue = 50;
+uint16_t snakes[SNAKES_COUNT];
+int snake = 0;
+int speed = 1;
+bool readSerial = false;
+
+int curve(int value, int difference, int maximum) {
+  if (value < maximum && increasing) {
+    value += difference;
+  } else if (value > 1) {
+    value -= difference;
+    increasing = false;
+  } else {
+    increasing = true;
+    value += difference;
+  }
+  return value;
+}
+
 void setupLanternPattern() {
   // Set up Serial for lantern pattern
   Serial1.begin(9600);
-  Serial1.setTimeout(10);
+  Serial1.setTimeout(20);
 }
-
-#define CIRCUMFERENCE 22
-#define SNAKES_COUNT 50
-#define READ_SERIAL false
-
-uint8_t hue = 50;
-uint16_t snakes[SNAKES_COUNT];
-int numberOfSparkles = 0;
-int newBrightness = 128;
-int newHue = hue;
-int snake = 0;
-int speed = 1;
-
-void setSettingsFromSerial() {
-//    if(Serial1.available()) {
-//      JsonObject& root = jsonBuffer.parseObject(Serial1);
-//      root.prettyPrintTo(Serial);
-//  
-//      float hueDecimal = root["hue"];
-//      if (hueDecimal) {
-//        newHue = hueDecimal * 255;
-//      }
-//  
-//      float brightnessDecimal = root["brightness"];
-//      if (brightnessDecimal) {
-//        newBrightness = brightnessDecimal * 255.0;
-//        if (newBrightness < 3.0) newBrightness = 0;
-//      }
-//  
-//      float speedDecimal = root["speed"];
-//      if (speedDecimal) {
-//        speed = (speedDecimal * 50);
-//      }
-//    }
-
-  EVERY_N_MILLISECONDS(10) {
-    if (newBrightness > FastLED.getBrightness()) {
-      FastLED.setBrightness(FastLED.getBrightness() + 1);
-    } else if (newBrightness < FastLED.getBrightness()) {
-      FastLED.setBrightness(FastLED.getBrightness() - 1);
-    }
-  }
-
-  EVERY_N_MILLISECONDS(10) {
-    if (newHue > hue) {
-      hue++;
-    } else if (newHue < hue) {
-      hue--;
-    }
-  }
-}
-
-// The constants here have to change for APA102/WS2812B.
-// The timing depends on how many LEDs there are, especially if you're using WS2812B.
-#define FADE_MS 20
-#define SPARKLE_MS 60
-#define CURVE_MS 200
-#define SEED_SNAKES_MS 50
-#define MAX_SPARKLES_AS_A_PERCENTAGE_OF_NUM_LEDS 0.02
-#define MOVE_SNAKES_MS 40
 
 void lanternPattern() {
-  if (READ_SERIAL) {
-    setSettingsFromSerial();
+  if (readSerial) {
+    if(Serial1.available()) {
+      StaticJsonBuffer<200> jsonBuffer; 
+      JsonObject& root = jsonBuffer.parseObject(Serial1);
+      root.prettyPrintTo(Serial);
+      
+      float hueDecimal = root["hue"];
+      if (hueDecimal) {
+        hue = hueDecimal * 255;
+      }
+      
+      float brightnessDecimal = root["brightness"];
+      if (brightnessDecimal) {
+       brightnessDecimal = brightnessDecimal * 255.0;
+       if (brightnessDecimal < 3.0) brightnessDecimal = 0;
+       FastLED.setBrightness(brightnessDecimal);
+      }
+      
+      float speedDecimal = root["speed"];
+      if (speedDecimal) {
+        speed = (speedDecimal * 50);
+      }
+
+      jsonBuffer.clear();
+    }
   } else {
     EVERY_N_MILLISECONDS(100) {
       hue -= 1;
     }
   }
 
-  EVERY_N_MILLISECONDS(MOVE_SNAKES_MS) {
+  EVERY_N_MILLISECONDS(40) {
     for (int snakeIndex = 0; snakeIndex < SNAKES_COUNT; snakeIndex++) {
       uint16_t snakeHead = snakes[snakeIndex];
       if (snakeHead > 0) {
         // Snake hue
-        leds[snakeHead] = CHSV(hue + snakeHead / 10, 240, 255);
+        leds[snakeHead] = CHSV(hue + snakeHead / 15 - 20, 240, 200);
         // Snake movement
         snakes[snakeIndex] += CIRCUMFERENCE;
         // Reset snake
@@ -90,28 +76,30 @@ void lanternPattern() {
     }
   }
 
-  EVERY_N_MILLISECONDS(SEED_SNAKES_MS) {
-    float randomness = 0.5;
+  EVERY_N_MILLISECONDS(50) {
     // Snake seeding - randomly start a snake based on numberOfSparkles
-    if (random16(NUM_LEDS * randomness) < numberOfSparkles) {
-      snakes[snake] = random(CIRCUMFERENCE);
+    if (random16(NUM_LEDS * 0.3) < numberOfSparkles) {
+      snakes[snake] = random(7);
       snake++;
       if (snake >= SNAKES_COUNT) snake = 0;
     }
   }
 
-  EVERY_N_MILLISECONDS(FADE_MS) {
-    nscale8(leds, NUM_LEDS, 255 - speed);
-  }
+//  EVERY_N_MILLISECONDS(20) {
+    // todo nscale8_video
+    fadeToBlackBy(leds, NUM_LEDS, speed);
+//  }
 
-  EVERY_N_MILLISECONDS(SPARKLE_MS) {
+//  EVERY_N_MILLISECONDS(60) {
     for (int i = 0; i < numberOfSparkles; i++) {
       int pos = random16(NUM_LEDS);
-      if (!leds[pos]) leds[pos] = CHSV(hue + (pos / 15), 255, 128);
+      if (!leds[pos]) {
+        leds[pos] = CHSV(hue + (pos / 15), 255, 128);
+      }
     }
-  }
+//  }
 
-  EVERY_N_MILLISECONDS(CURVE_MS) {
-    numberOfSparkles = curve(numberOfSparkles, 4, NUM_LEDS * MAX_SPARKLES_AS_A_PERCENTAGE_OF_NUM_LEDS);
+  EVERY_N_MILLISECONDS(200) {
+    numberOfSparkles = curve(numberOfSparkles, 4, NUM_LEDS * 0.02);
   }
 }
