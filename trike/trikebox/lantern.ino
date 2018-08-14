01fb7f154c7f4ef058c221e9c679d026db0a8e86
Elliott Kember <elliott.kember@gmail.com>
2018-08-13 22:12:57 -0700

    New lantern

--- trike/trikebox/lantern.ino
+++ trike/trikebox/lantern.ino
@@ -1,22 +1,9 @@
 namespace Lantern {
-  const int snakesCount = 50;
+  const int snakesCount = 10;
   const int circumference = LEDS_PER_ROW;
   int numberOfSparkles = 0;
   bool increasing = true;
   uint8_t hue = 50;
-
-  int curve(int value, int difference, int maximum) {
-    if (value < maximum && increasing) {
-      value += difference;
-    } else if (value > 1) {
-      value -= difference;
-      increasing = false;
-    } else {
-      increasing = true;
-      value += difference;
-    }
-    return value;
-  }
 
   uint16_t snakes[snakesCount];
   int snake = 0;
@@ -28,14 +15,26 @@ namespace Lantern {
   }
 
   void lanternPattern() {
+    numberOfSparkles = beatsin16(15, 4, N_LEDS * 0.03);
+
+    EVERY_N_MILLISECONDS(100) {
+      hue -= 1;
+    }
+    
     EVERY_N_MILLISECONDS(40) {
       for (int i = 0; i < snakesCount; i++) {
         uint16_t j = snakes[i];
         if (j > 0) {
-          // Different hue for shooting stars
-          Soulmate::led_arr[j] = CHSV(hue + j / 10 + 10, 240, 255);
+          if (j > 1) {
+            Soulmate::led_arr[j-1] = CHSV(hue + j / 10 + 10, 180, 100);
+          }
+          Soulmate::led_arr[j] = CHSV(hue + j / 10 + 10, 180, 180);
+          if (j < N_LEDS - 1) {
+            Soulmate::led_arr[j+1] = CHSV(hue + j / 10 + 10, 180, 100);
+          }
 
-          snakes[i] += circumference+1;
+          // angle
+          snakes[i] += circumference + 1;
           if (snakes[i] >= N_LEDS) snakes[i] = 0;
         }
       }
@@ -45,9 +44,6 @@ namespace Lantern {
       if (random16(N_LEDS * 0.4) < numberOfSparkles) {
         shootingStar();
       }
-    }
-
-    EVERY_N_MILLISECONDS(10) {
       fade_raw(Soulmate::led_arr, N_LEDS, 1);
     }
 
@@ -58,14 +54,6 @@ namespace Lantern {
           Soulmate::led_arr[pos] = CHSV(hue + (pos / 15), 255, 128);
         }
       }
-    }
-
-    EVERY_N_MILLISECONDS(200) {
-      numberOfSparkles = curve(numberOfSparkles, 4, N_LEDS * 0.03);
-    }
-
-    EVERY_N_MILLISECONDS(100) {
-      hue -= 1;
     }
   }
 }
